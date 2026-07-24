1172b44602d6af68218bd18bbf5300bce162f67d
me___oov <snehagiri157@gmail.com>
2026-07-24 19:59:53 +0545

    Delete backend directory

--- backend/src/main/java/com/lumo/exception/DuplicateEmailException.java
+++
@@ -1,7 +0,0 @@
-package com.lumo.exception;
-
-public class DuplicateEmailException extends RuntimeException {
-    public DuplicateEmailException(String message) {
-        super(message);
-    }
-}
